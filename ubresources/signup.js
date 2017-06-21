b85b19a2f465d97a54d98b7123beee0e76d9c3da
Delveri Munang <chickdelveri@gmail.com>
2017-06-21 11:46:13 -0400

    Added generators for random usernames and passwords

--- ubresources/signup.js
+++ ubresources/signup.js
@@ -10,26 +10,53 @@ chrome.setDefaultService(service);
 var driver = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
+function randomPassword(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+    }
+    return pass;
+}
+
+function randomUsername(length){
+	var chars = "abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOP1234567890";
+    var username = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        username += chars.charAt(i);
+    }
+    return username;
+}
+
+function randomEmail(length){
+	var chars = "abcdefghijklmnopqrstuvwxyz_1234567890";
+    var email = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        email += chars.charAt(i);
+    }
+    return username + "@ubresources.com";
+}
+
+
+
+
 
 driver.get('http://ubresources.com/join');
-
- // for (var i = 0; i < 20 ; i++) {
- // 	driver.findElement(By.name('username')).sendKeys(i);
-	// driver.findElement(By.name('password')).sendKeys(i);
-	// driver.findElement(By.className('btn-success')).click();
-	// if (i==18) {
-	// 	driver.findElement(By.name('user')).sendKeys('admin@localhost.com');
-	// 	driver.findElement(By.name('pass')).sendKeys('admin');
-	// 	driver.findElement(By.className('btn-primary')).click();
-	// }
-	
- // }
-for(var i=0 ; i< 5 ; i++){
-driver.findElement(By.name('username')).sendKeys('root');
-driver.findElement(By.name('email')).sendKeys('root@localhost.com');
-driver.findElement(By.name('password')).sendKeys('root1234');
-driver.findElement(By.name('password')).sendKeys('root1234');
-driver.findElement(By.className('btn-success')).click();
+for(var i=0 ; i< 2 ; i++){
+var username = randomUsername(8);
+driver.findElement(By.name('username')).sendKeys(username);
+console.log(username);
+var email = randomEmail(6);
+driver.findElement(By.name('email')).sendKeys(email);
+console.log(email);
+var password = randomPassword(8);										// Generate random password
+driver.findElement(By.name('password')).sendKeys(password);
+console.log(password);
+driver.findElement(By.name('password_confirmation')).sendKeys(password);
+driver.findElement(By.id('create-account')).click();
 }
 driver.wait(until.titleIs('webdriver - Google Search'), 5000);
 driver.quit();
